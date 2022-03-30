213cace1f8a4e8a5d598f4f1d3aefb20f94dda9a
Roberlan Carvalho <roberlan.carvalho@gmail.com>
2022-03-30 17:08:13 -0300

    add enums domain

---
+++ src/main/java/com/tecnorth/helpdesk/domain/enums/Priority.java
@@ -0,0 +1,38 @@
+package com.tecnorth.helpdesk.domain.enums;
+
+public enum Status {
+
+    OPEN( 0, "ROLE_ADMIN" ), PROGRESS( 1, "ROLE_PROGRESS" ), CLOSED( 2, "ROLE_CLOSED" );
+
+
+    private Integer code;
+    private String description;
+
+    Status(Integer code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public static Status toEnum(Integer cod) {
+        if (cod == null) {
+            return null;
+        }
+        for (Status x : Status.values()) {
+            if (cod.equals( x.getCode() )) {
+                return x;
+            }
+        }
+
+        throw new IllegalArgumentException( "Perfil inválido" );
+
+    }
+
+}
---
+++ src/main/java/com/tecnorth/helpdesk/domain/enums/Profile.java
@@ -0,0 +1,4 @@
+package com.tecnorth.helpdesk.domainenums;
+
+public enum Profile {
+}
